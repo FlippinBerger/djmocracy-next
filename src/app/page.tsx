19e7acafc41b6677d1b89d026d78a0ab7debaefe
Chris Berger <flippinberger@gmail.com>
2023-10-22 22:30:40 -0600

    Cleanup boilerplate; Add prettier and eslint rules

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <h1 className="text-4xl">Hello world</h1>
-        <p>Put some content in here</p>
-      </div>
-    </main>
-  )
+    <div>
+      <h1 className='text-4xl'>Welcome to DJmocracy</h1>
+      <p>Put some content in here</p>
+    </div>
+  );
 }
